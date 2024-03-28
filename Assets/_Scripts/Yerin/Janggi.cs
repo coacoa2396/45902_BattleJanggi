f7b3bedea257914b53ea1e1e53344ed5e40b9ee6
Mellling <154119653+Mellling@users.noreply.github.com>
2024-03-28 17:11:00 +0900

    feat: 장기말 프리팹 생성 및 수정

---
+++ Assets/_Scripts/Yerin/Janggi.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 개발자: Yerin
+/// 
+/// 장기말의 기능 구현
+/// </summary>
+public class Janggi : MonoBehaviour
+{
+
+    // 장기말의 움직임
+    // Spot한테
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

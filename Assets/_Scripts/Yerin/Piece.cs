a71ef56320d1ca30c19ad6a76b2655c2f92c91ea
Mellling <154119653+Mellling@users.noreply.github.com>
2024-03-29 11:37:50 +0900

    feat: 장기말 기본 기능 구현
    마우스과 장기말의 상호작용 상태에 따라 장기말 오브젝트의 색깔 변형 구현

--- Assets/_Scripts/Yerin/Piece.cs
+++ Assets/_Scripts/Yerin/Piece.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -9,17 +10,55 @@ using UnityEngine.EventSystems;
 /// 
 /// 장기말의 기능 구현
 /// </summary>
-public class Piece : MonoBehaviour, IPointerClickHandler
+public class Piece : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    private Spot[,] JanggiSituation;    // 현재 장기판 위 말의 상황을 받아 올 배열
+
+    Material pieceMaterial; // 해당 장기말의 Material을 받을 변수
+
+    private void Start()
+    {
+        pieceMaterial = gameObject.GetComponent<Renderer>().material;
+    }
+
     /// <summary>
     /// 해당 장기말 선택 시 장기판의 전체적인 상황 받아옴
+    /// 플레이어가 장기말을 선택 시 해당 오브젝트의 색을 빨간색으로 변경
     /// </summary>
     /// <param name="eventData"></param>
     public void OnPointerClick(PointerEventData eventData)
     {
-        //JanggiLogic
+        JanggiSituation = Manager.JanggiLogic.JanggiLogicSituation;
+
+        if (JanggiSituation == null)
+        {
+            Debug.Log("Get JanggiLogic Fail");
+        }
+
+        pieceMaterial.color = Color.red;
+    }
+    /// <summary>
+    /// 플레이어가 해당 장기말 위에 마우스를 올릴 시 오브젝트의 색을 노란색으로 변경
+    /// 선택할 수 있음을 알려주기 위해
+    /// </summary>
+    /// <param name="eventData"></param>
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        pieceMaterial.color = Color.yellow;
     }
 
-    // 클릭될 시 장기판 상황 가져오기 (포인터 이벤트 사용하기)
-    // 클릭될 시 장기말 색깔 변형(일단은)
+    /// <summary>
+    /// 플레이어가 해당 장기말을 선택하지 않고 나왔을 시 오브젝트의 색을 원래대로 변경
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (pieceMaterial.color == Color.red)
+        {
+            return;
+        }
+
+        pieceMaterial.color = Color.white;
+    }
 }

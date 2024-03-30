1c9b969779a3a1b6ac6ecf03a2043b02f92ae6c1
Yu <kgadbcksrb@gmail.com>
2024-03-30 17:40:10 +0900

    feat : 스크립터블 오브젝트 준비

---
+++ Assets/_Scripts/Yu/FPSPiece.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// ChanGyu, Yerin
+/// </summary>
+public class FPSPiece : MonoBehaviour
+{
+    
+}

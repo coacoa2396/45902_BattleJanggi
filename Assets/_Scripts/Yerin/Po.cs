fc2de2c6df1da748e8dd2516054f4fc455119887
Mellling <154119653+Mellling@users.noreply.github.com>
2024-03-30 15:10:00 +0900

    feat: 포 FindCanGo 함수 구현

--- Assets/_Scripts/Yerin/Po.cs
+++ Assets/_Scripts/Yerin/Po.cs
@@ -27,47 +27,41 @@ public class Po : Piece
     {
         // 말의 x 좌표 기준 더 작은 쪽 검사
 
-        if (currentPos['x'] == 0)
-        {
-            Debug.Log("Null");
-            return;
-        }
-
         for (int x = currentPos['x'] - 1; x >= 0 ; x--)
         {
             bool checkStop = false;
 
-            Debug.Log("In");
-
-            if (
-                JanggiSituation[currentPos['z'], x].OnPiece &&
-                !JanggiSituation[currentPos['z'], x].WhatPiece.PieceName.Equals("Po"))
+            if (JanggiSituation[currentPos['z'], x].OnPiece)
             {
-                //Debug.Log("Help");
-                Debug.Log("1");
+                if (JanggiSituation[currentPos['z'], x].WhatPiece.PieceName.Equals("Po"))
+                {
+                    Debug.Log("This is Po");
+                    break;
+                }
+
                 x--;
+
                 for (; x >= 0; x--)
                 {
-                    Debug.Log($"({currentPos['z']},{x})");
-                    JanggiSituation[currentPos['z'], x].gameObject.GetComponent<Renderer>().material.color = Color.red;
-
                     if (JanggiSituation[currentPos['z'], x].OnPiece)
                     {
-                        Debug.Log("2");
                         if (JanggiSituation[currentPos['z'], x].WhatPiece.PieceName.Equals("Po") || JanggiSituation[currentPos['z'], x].WhosePiece.Equals(WhosPiece))
                         {
-                            Debug.Log("3");
                             checkStop = true;
                             break;
                         }
                         else if (!JanggiSituation[currentPos['z'], x].WhosePiece.Equals(WhosPiece))
                         {
-                            Debug.Log("4");
                             checkStop = true;
-                            Debug.Log($"({currentPos['z']},{x})");
+                            Debug.Log($"({currentPos['z']},{x}) 2");
                             JanggiSituation[currentPos['z'], x].gameObject.GetComponent<Renderer>().material.color = Color.red;
                             break;
                         }
+                    }
+                    else
+                    {
+                        Debug.Log($"({currentPos['z']},{x}) 1");
+                        JanggiSituation[currentPos['z'], x].gameObject.GetComponent<Renderer>().material.color = Color.red;
                     }
                 }
             }
@@ -78,18 +72,24 @@ public class Po : Piece
             }
         }
 
-        /*// 말의 x 좌표 기준 더 큰 쪽 검사
+        // 말의 x 좌표 기준 더 큰 쪽 검사
 
         for (int x = currentPos['x'] + 1; x < 9; x++)
         {
             bool checkStop = false;
 
-            if (JanggiSituation[currentPos['z'], x].OnPiece && !JanggiSituation[currentPos['z'], x].WhatPiece.PieceName.Equals("Po"))
+            if (JanggiSituation[currentPos['z'], x].OnPiece)
             {
+                if (JanggiSituation[currentPos['z'], x].WhatPiece.PieceName.Equals("Po"))
+                {
+                    Debug.Log("This is Po");
+                    break;
+                }
+
+                x++;
+
                 for (; x < 9; x++)
                 {
-                    JanggiSituation[currentPos['z'], x].gameObject.GetComponent<Renderer>().material.color = Color.red;
-
                     if (JanggiSituation[currentPos['z'], x].OnPiece)
                     {
                         if (JanggiSituation[currentPos['z'], x].WhatPiece.PieceName.Equals("Po") || JanggiSituation[currentPos['z'], x].WhosePiece.Equals(WhosPiece))
@@ -100,9 +100,15 @@ public class Po : Piece
                         else if (!JanggiSituation[currentPos['z'], x].WhosePiece.Equals(WhosPiece))
                         {
                             checkStop = true;
+                            Debug.Log($"({currentPos['z']},{x}) 2");
                             JanggiSituation[currentPos['z'], x].gameObject.GetComponent<Renderer>().material.color = Color.red;
                             break;
                         }
+                    }
+                    else
+                    {
+                        Debug.Log($"({currentPos['z']},{x}) 1");
+                        JanggiSituation[currentPos['z'], x].gameObject.GetComponent<Renderer>().material.color = Color.red;
                     }
                 }
             }
@@ -115,16 +121,22 @@ public class Po : Piece
 
         // 말의 z 좌표 기준 더 작은 쪽 검사
 
-        for (int z = 0; z < currentPos['z']; z++)
+        for (int z = currentPos['z'] + 1; z < 10; z++)
         {
             bool checkStop = false;
 
-            if (JanggiSituation[z, currentPos['x']].OnPiece && !JanggiSituation[z, currentPos['x']].WhatPiece.PieceName.Equals("Po"))
+            if (JanggiSituation[z, currentPos['x']].OnPiece)
             {
-                for (; z < currentPos['z']; z++)
+                if (JanggiSituation[z, currentPos['x']].WhatPiece.PieceName.Equals("Po"))
                 {
-                    JanggiSituation[z, currentPos['x']].gameObject.GetComponent<Renderer>().material.color = Color.red;
+                    Debug.Log("This is Po");
+                    break;
+                }
 
+                z++;
+
+                for (; z < 10; z++)
+                {
                     if (JanggiSituation[z, currentPos['x']].OnPiece)
                     {
                         if (JanggiSituation[z, currentPos['x']].WhatPiece.PieceName.Equals("Po") || JanggiSituation[z, currentPos['x']].WhosePiece.Equals(WhosPiece))
@@ -135,9 +147,15 @@ public class Po : Piece
                         else if (!JanggiSituation[z, currentPos['x']].WhosePiece.Equals(WhosPiece))
                         {
                             checkStop = true;
+                            Debug.Log($"({currentPos['x']},{z}) 2");
                             JanggiSituation[z, currentPos['x']].gameObject.GetComponent<Renderer>().material.color = Color.red;
                             break;
                         }
+                    }
+                    else
+                    {
+                        Debug.Log($"({currentPos['x']},{z}) 1");
+                        JanggiSituation[z, currentPos['x']].gameObject.GetComponent<Renderer>().material.color = Color.red;
                     }
                 }
             }
@@ -150,16 +168,22 @@ public class Po : Piece
 
         // 말의 z 좌표 기준 더 큰 쪽 검사
 
-        for (int z = currentPos['z'] + 1; z < 10; z++)
+        for (int z = currentPos['z'] - 1; z >= 0; z--)
         {
             bool checkStop = false;
 
-            if (JanggiSituation[z, currentPos['x']].OnPiece && !JanggiSituation[z, currentPos['x']].WhatPiece.PieceName.Equals("Po"))
+            if (JanggiSituation[z, currentPos['x']].OnPiece)
             {
-                for (; z < 10; z++)
+                if (JanggiSituation[z, currentPos['x']].WhatPiece.PieceName.Equals("Po"))
                 {
-                    JanggiSituation[z, currentPos['x']].gameObject.GetComponent<Renderer>().material.color = Color.red;
+                    Debug.Log("This is Po");
+                    break;
+                }
 
+                z--;
+
+                for (; z >= 0; z--)
+                {
                     if (JanggiSituation[z, currentPos['x']].OnPiece)
                     {
                         if (JanggiSituation[z, currentPos['x']].WhatPiece.PieceName.Equals("Po") || JanggiSituation[z, currentPos['x']].WhosePiece.Equals(WhosPiece))
@@ -170,9 +194,15 @@ public class Po : Piece
                         else if (!JanggiSituation[z, currentPos['x']].WhosePiece.Equals(WhosPiece))
                         {
                             checkStop = true;
+                            Debug.Log($"({currentPos['x']},{z}) 2");
                             JanggiSituation[z, currentPos['x']].gameObject.GetComponent<Renderer>().material.color = Color.red;
                             break;
                         }
+                    }
+                    else
+                    {
+                        Debug.Log($"({currentPos['x']},{z}) 1");
+                        JanggiSituation[z, currentPos['x']].gameObject.GetComponent<Renderer>().material.color = Color.red;
                     }
                 }
             }
@@ -181,6 +211,6 @@ public class Po : Piece
             {
                 break;
             }
-        }*/
+        }
     }
 }

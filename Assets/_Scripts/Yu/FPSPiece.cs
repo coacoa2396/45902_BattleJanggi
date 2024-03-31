f75c55fc148afeeb5c7660dc193ad3acd6cc0f22
Yu <kgadbcksrb@gmail.com>
2024-03-31 22:50:45 +0900

    feat : Jump가 왜 안될까

--- Assets/_Scripts/Yu/FPSPiece.cs
+++ Assets/_Scripts/Yu/FPSPiece.cs
@@ -20,6 +20,7 @@ public class FPSPiece : MonoBehaviour
     [Header("Property")]
     [SerializeField] float movePower;
     [SerializeField] float maxMoveSpeed;
+    [SerializeField] float breakPower;
 
     [SerializeField] float jumpPower;
 
@@ -32,34 +33,50 @@ public class FPSPiece : MonoBehaviour
 
     // 이동
     public void Move()
-    {
-
-        rigid.AddForce(moveDir * movePower * Time.fixedDeltaTime, ForceMode.Impulse);      // 인풋액션으로 받아온 평면좌표방향으로 movePower만큼의 힘을 준다
-
-        if (rigid.velocity.sqrMagnitude > maxMoveSpeed * maxMoveSpeed)
+    {        
+        // x방향(좌우) 체크
+        if (moveDir.x < 0 && rigid.velocity.x > -maxMoveSpeed)      // 왼쪽 이동
         {
-            rigid.velocity = rigid.velocity.normalized * maxMoveSpeed;
+            rigid.AddForce(Vector3.right * moveDir.x * movePower * Time.fixedDeltaTime);
+        }
+        else if (moveDir.x > 0 && rigid.velocity.x < maxMoveSpeed)  // 오른쪽 이동
+        {
+            rigid.AddForce(Vector3.right * moveDir.x * movePower * Time.fixedDeltaTime);
+        }
+        else if (moveDir.x == 0 && rigid.velocity.x > 0.1f)         // 입력이 없을 시 반대로 힘을 줘서 빠르게 멈추게 하여 조작감 개선
+        {
+            rigid.AddForce(Vector3.left * breakPower * Time.fixedDeltaTime);
+        }
+        else if (moveDir.x == 0 && rigid.velocity.x < -0.1f)        // 입력이 없을 시 반대로 힘을 줘서 빠르게 멈추게 하여 조작감 개선
+        {
+            rigid.AddForce(Vector3.right * breakPower * Time.fixedDeltaTime);
         }
 
-        //if (moveDir.x < 0 && rigid.velocity.x > -maxMoveSpeed)
-        //{
-        //    rigid.AddForce(Vector3.left * movePower * moveDir.x * Time.fixedDeltaTime);
-        //}
-        //else if (moveDir.x > 0 && rigid.velocity.x < maxMoveSpeed)
-        //{
-        //    rigid.AddForce(Vector3.left * movePower * moveDir.x * Time.fixedDeltaTime);
-        //}
+        // z방향(앞뒤) 체크
+        if (moveDir.z < 0 && rigid.velocity.z > -maxMoveSpeed)      // 뒤 이동
+        {
+            rigid.AddForce(Vector3.forward * moveDir.z * movePower * Time.fixedDeltaTime);
+        }
+        else if (moveDir.z > 0 && rigid.velocity.z < maxMoveSpeed)  // 앞 이동
+        {
+            rigid.AddForce(Vector3.forward * moveDir.z * movePower * Time.fixedDeltaTime);
+        }
+        else if (moveDir.z == 0 && rigid.velocity.z > 0.1f)         // 입력이 없을 시 반대로 힘을 줘서 조작감 개선
+        {
+            rigid.AddForce(Vector3.back * breakPower * Time.fixedDeltaTime);
+        }
+        else if (moveDir.z == 0 && rigid.velocity.z < -0.1f)        // 입력이 없을 시 반대로 힘을 줘서 조작감 개선
+        {
+            rigid.AddForce(Vector3.forward * breakPower * Time.fixedDeltaTime);
+        }
 
-        //if (moveDir.z < 0 && rigid.velocity.z > -maxMoveSpeed)
-        //{
-        //    rigid.AddForce(Vector3.forward * movePower * moveDir.z * Time.fixedDeltaTime);
-        //}
-        //else if (moveDir.z > 0 && rigid.velocity.z < maxMoveSpeed)
-        //{
-        //    rigid.AddForce(Vector3.forward * movePower * moveDir.z * Time.fixedDeltaTime);
-        //}
+        if (rigid.velocity.sqrMagnitude > maxMoveSpeed * maxMoveSpeed)  // 최고속도 제한걸기
+        {
+            rigid.velocity = rigid.velocity.normalized * maxMoveSpeed;
+        }        
     }
     
+    // 인풋액션에서 받아오는 정보
     private void OnMove(InputValue value)
     {
         Vector2 input = value.Get<Vector2>();
@@ -71,7 +88,18 @@ public class FPSPiece : MonoBehaviour
     // 점프
     private void OuJump(InputValue value)
     {
+        Debug.Log("온점프");
+        if (value.isPressed)
+            Jump();
+    }
 
+    /// <summary>
+    /// 점프를 실행하는 함수
+    /// </summary>
+    void Jump()
+    {
+        Debug.Log("점프");                
+        rigid.AddForce(Vector3.up * jumpPower * Time.fixedDeltaTime, ForceMode.Impulse);
     }
 
     /// <summary>
@@ -82,7 +110,7 @@ public class FPSPiece : MonoBehaviour
     {
         hp -= damage;
 
-        if (hp > 0)     // 체력이 0이상이면 리턴
+        if (hp > 0)     // 체력이 0초과면 리턴
             return;
 
         Die();

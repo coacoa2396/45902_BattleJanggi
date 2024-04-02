7ef81ec4b7b5a0034985db84100b35e0338f5794
Yu <kgadbcksrb@gmail.com>
2024-04-02 13:24:57 +0900

    feat : FPS이동 수정
    rigid,AddForce에서 캐릭터컨트롤러로 이동방법 수정

--- Assets/_Scripts/Yu/FPSPiece.cs
+++ Assets/_Scripts/Yu/FPSPiece.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 
 
 /// <summary>
@@ -14,22 +15,22 @@ public class FPSPiece : MonoBehaviour
     [SerializeField] float hp;
 
     [Header("Componemt")]
-    [SerializeField] InputActionAsset inputAction;
+    [SerializeField] CharacterController controller;
     [SerializeField] Rigidbody rigid;
     [SerializeField] Weapon weapon;
 
     [Header("Property")]
-    [SerializeField] float movePower;
-    [SerializeField] float maxMoveSpeed;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpSpeed;
     [SerializeField] float breakPower;
-
-    [SerializeField] float jumpPower;
-
+    [SerializeField] float ySpeed;
+    
     private Vector3 moveDir;
 
     private void FixedUpdate()
     {
         Move();
+        JumpMove();
     }
 
     /// <summary>
@@ -37,53 +38,8 @@ public class FPSPiece : MonoBehaviour
     /// </summary>
     public void Move()
     {
-        if (moveDir.x == 0 && rigid.velocity.magnitude < 0.001f)
-        {
-            rigid.velocity = Vector3.zero;
-        }
-
-        // x방향(좌우) 체크
-        if (moveDir.x < 0 && rigid.velocity.x > -maxMoveSpeed)      // 왼쪽 이동
-        {
-            rigid.AddForce(Vector3.right * moveDir.x * movePower);
-        }
-        else if (moveDir.x > 0 && rigid.velocity.x < maxMoveSpeed)  // 오른쪽 이동
-        {
-            rigid.AddForce(Vector3.right * moveDir.x * movePower);
-        }
-        else if (moveDir.x == 0 && rigid.velocity.x > 0.001f)         // 입력이 없을 시 반대로 힘을 줘서 빠르게 멈추게 하여 조작감 개선
-        {
-            rigid.AddForce(Vector3.left * breakPower);
-        }
-        else if (moveDir.x == 0 && rigid.velocity.x < -0.001f)        // 입력이 없을 시 반대로 힘을 줘서 빠르게 멈추게 하여 조작감 개선
-        {
-            rigid.AddForce(Vector3.right * breakPower);
-        }
-
-        // z방향(앞뒤) 체크
-        if (moveDir.z < 0 && rigid.velocity.z > -maxMoveSpeed)      // 뒤 이동
-        {
-            rigid.AddForce(Vector3.forward * moveDir.z * movePower);
-        }
-        else if (moveDir.z > 0 && rigid.velocity.z < maxMoveSpeed)  // 앞 이동
-        {
-            rigid.AddForce(Vector3.forward * moveDir.z * movePower);
-        }
-        else if (moveDir.z == 0 && rigid.velocity.z > 0.001f)         // 입력이 없을 시 반대로 힘을 줘서 조작감 개선
-        {
-            rigid.AddForce(Vector3.back * breakPower);
-        }
-        else if (moveDir.z == 0 && rigid.velocity.z < -0.001f)        // 입력이 없을 시 반대로 힘을 줘서 조작감 개선
-        {
-            rigid.AddForce(Vector3.forward * breakPower);
-        }
-
-        if (rigid.velocity.sqrMagnitude > maxMoveSpeed * maxMoveSpeed)  // 최고속도 제한걸기
-        {
-            rigid.velocity = rigid.velocity.normalized * maxMoveSpeed;
-        }
-
-
+        controller.Move(transform.right * moveDir.x * moveSpeed * Time.deltaTime);
+        controller.Move(transform.forward * moveDir.z * moveSpeed * Time.deltaTime);
     }
 
     /// <summary>
@@ -130,17 +86,23 @@ public class FPSPiece : MonoBehaviour
     private void OnJump(InputValue value)
     {
         Debug.Log("온점프");
-        if (value.isPressed)
-            Jump();
+        ySpeed = jumpSpeed;
     }
 
     /// <summary>
     /// 점프를 실행하는 함수
     /// </summary>
-    void Jump()
+    void JumpMove()
     {
         Debug.Log("점프");
-        rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (controller.isGrounded)
+        {
+            ySpeed = 0f;
+        }
+
+        controller.Move(Vector3.up * ySpeed * Time.deltaTime);      // 여기서 deltaTime을 한번 더 곱해주니까 제곱이 된다
     }
 
     /// <summary>

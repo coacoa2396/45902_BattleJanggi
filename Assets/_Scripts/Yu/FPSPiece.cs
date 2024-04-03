fa08791229e6db7c0f4adf47836bf3ba7aaddf8f
Mellling <154119653+Mellling@users.noreply.github.com>
2024-04-03 15:45:13 +0900

    feat: FPS씬 플레이어 기본 애니메이션

--- Assets/_Scripts/Yu/FPSPiece.cs
+++ Assets/_Scripts/Yu/FPSPiece.cs
@@ -29,9 +29,7 @@ public class FPSPiece : MonoBehaviour
 
     [Header("Property")]
     [SerializeField] float moveSpeed;
-    [SerializeField] float jumpSpeed;
     [SerializeField] float breakPower;
-    [SerializeField] float ySpeed;
 
     bool isGround;  // 플레이어의 땅 위 여부
     bool isWalking; // 플레이어의 걷기 여부
@@ -47,7 +45,6 @@ public class FPSPiece : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
-        //JumpMove();
     }
 
     /// <summary>
@@ -130,24 +127,7 @@ public class FPSPiece : MonoBehaviour
     // 점프
     private void OnJump(InputValue value)
     {       
-        ySpeed = jumpSpeed;
-
         isJumping = true;
-    }
-
-    /// <summary>
-    /// 점프를 실행하는 함수
-    /// </summary>
-    void JumpMove()
-    {        
-        ySpeed += Physics.gravity.y * Time.deltaTime;
-
-        if (controller.isGrounded)
-        {
-            ySpeed = 0f;
-        }
-
-        controller.Move(Vector3.up * ySpeed * Time.deltaTime);      // 여기서 deltaTime을 한번 더 곱해주니까 제곱이 된다
     }
 
     /// <summary>
@@ -196,10 +176,7 @@ public class FPSPiece : MonoBehaviour
         {
             isGround = true;
 
-            if (isJumping)
-            {
-                isJumping = false;
-            }
+            isJumping = false;
         }
     }
 
@@ -209,5 +186,10 @@ public class FPSPiece : MonoBehaviour
         {
             isGround = false;
         }
+    }
+
+    public void makeIsJumpingFalse()
+    {
+        isJumping = false;
     }
 }

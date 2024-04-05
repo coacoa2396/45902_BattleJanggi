a9dd72dc59438bac242bad1e628c3c64e17861ee
Mellling <154119653+Mellling@users.noreply.github.com>
2024-04-05 14:00:00 +0900

    feat: FPS 졸(활) 구현 완료, FPS 사(수리검) 구현 완료

--- Assets/_Scripts/Yerin/FPSSa.cs
+++ Assets/_Scripts/Yerin/FPSSa.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+/// <summary>
+/// 개발자: Yerin
+/// 
+/// 장기말 사의 스킬
+/// </summary>
 public class FPSSa : FPSPiece
 {
-    // Start is called before the first frame update
-    void Start()
+    Coroutine skill;
+
+    bool canUseSkill = true;
+
+    IEnumerator Skill()
     {
-        
+        yield return new WaitForSeconds(5f);
+
+        gameObject.layer = LayerMask.NameToLayer("Default");
+        canUseSkill = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSkill(InputValue value)
     {
-        
+        if (canUseSkill)
+        {
+            gameObject.layer = LayerMask.NameToLayer("Invisible");
+            skill = StartCoroutine(Skill());
+
+            canUseSkill = false;
+        }
     }
+
 }

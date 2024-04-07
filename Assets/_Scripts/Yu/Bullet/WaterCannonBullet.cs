36e75470b5113bc5fc13dec522bfbbd8eda96474
Yu <kgadbcksrb@gmail.com>
2024-04-07 17:26:45 +0900

    feat : 장기말(상) 물대포 기본구현

---
+++ Assets/_Scripts/Yu/Bullet/WaterCannonBullet.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 제작자 : ChanGyu
+/// 물대포에서 나가는 탄환
+/// </summary>
+public class WaterCannonBullet : Bullet
+{
+    [SerializeField] WaterCannonImpact explodeEffect;        // 탄환이 닿았을 때 나오는 이팩트
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        Rigid.velocity = transform.forward * Speed;
+    }
+
+    protected override void OnCollisionEnter(Collision collision)
+    {
+        FPSPiece target;
+        collision.gameObject.TryGetComponent<FPSPiece>(out target);
+
+        target?.TakeDamage(Damage);
+        Manager.Pool.GetPool(explodeEffect, transform.position, Quaternion.LookRotation(collision.contacts[0].normal));
+
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        Rigid.velocity = Vector3.zero;
+    }
+}
---
+++ Assets/_Scripts/Yu/Gun/SangWaterCannon.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SangWaterCannon : Gun
+{
+    [SerializeField] Transform muzzlePoint;
+    [SerializeField] ParticleSystem muzzleFlash;
+
+    [SerializeField] float rate;        // 발사간격
+
+    bool checkFirable;
+
+    private void Start()
+    {
+        checkFirable = true;
+    }
+
+    public override void Fire()
+    {
+        if (!checkFirable)
+            return;
+
+        muzzleFlash.Play();
+        PooledObject PO = Manager.Pool.GetPool(Bullet, muzzlePoint.position, muzzlePoint.rotation);
+        Bullet initBullet = PO.GetComponent<Bullet>();
+
+        initBullet.Damage = Damage;
+        initBullet.Weapon = GetComponent<Weapon>();
+        StartCoroutine(FireChecker());
+    }
+
+    IEnumerator FireChecker()
+    {
+        checkFirable = false;
+        yield return new WaitForSeconds(rate);
+        checkFirable = true;
+    }
+}

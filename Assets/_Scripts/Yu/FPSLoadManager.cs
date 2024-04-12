3adfe6345daa9f95222883b25a40aff1f2ef7c29
Mellling <154119653+Mellling@users.noreply.github.com>
2024-04-12 21:29:54 +0900

    feat: 장기씬에서 FPS씬 전환 시 벽 생성 완료

--- Assets/_Scripts/Yu/FPSLoadManager.cs
+++ Assets/_Scripts/Yu/FPSLoadManager.cs
@@ -25,6 +25,48 @@ public class FPSLoadManager : MonoBehaviour
                         Wall instanceSang = Manager.Resource.Load<Wall>("Wall/SangWall_Complete");
                         Instantiate(instanceSang, spots.FPSLogicSituation[piece.z, piece.x].transform.position, Quaternion.identity);
                         break;
+                    case "Ma":
+                        Wall instanceMa = Manager.Resource.Load<Wall>("Wall/MaWall_Complete");
+
+                        float y = 0;
+
+                        if (piece.whosPiece.Equals("Cho"))
+                        {
+                            y = 180;
+                        }
+
+                        Instantiate(instanceMa, new Vector3(spots.FPSLogicSituation[piece.z, piece.x].transform.position.x, instanceMa.gameObject.transform.position.y, spots.FPSLogicSituation[piece.z, piece.x].transform.position.z), Quaternion.Euler(new Vector3(90, y, 0)));
+                        break;
+                    case "Po":
+                        Wall instancePo;
+                        y = 0;
+
+                        if (piece.whosPiece.Equals("Han"))
+                        {
+                            instancePo = Manager.Resource.Load<Wall>("Wall/POWALL(Han)_Complete");
+                        }
+                        else
+                        {
+                            instancePo = Manager.Resource.Load<Wall>("Wall/POWALL(Cho)_Complete");
+                            y = 180;
+                        }
+                        
+                        Instantiate(instancePo, spots.FPSLogicSituation[piece.z, piece.x].transform.position, Quaternion.Euler(new Vector3(0, y, 0)));
+                        break;
+                    case "Jang":
+
+                        break;
+                    default:
+                        Wall instanceWall = Manager.Resource.Load<Wall>("Wall/BaseWALL_Complete"); ;
+                        y = 0;
+
+                        if (piece.whosPiece.Equals("Cho"))
+                        {
+                            y = 180;
+                        }
+
+                        Instantiate(instanceWall, new Vector3(spots.FPSLogicSituation[piece.z, piece.x].transform.position.x, instanceWall.gameObject.transform.position.y, spots.FPSLogicSituation[piece.z, piece.x].transform.position.z), Quaternion.Euler(new Vector3(90, y, 0)));
+                        break;
 
                 }
                 // 해당 위치에 벽을 생성해주고

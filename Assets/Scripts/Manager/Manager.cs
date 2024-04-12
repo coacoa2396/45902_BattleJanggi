ecff96e764210a7c29a9bd24312d6518607f9be3
Yu <kgadbcksrb@gmail.com>
2024-04-12 20:45:41 +0900

    feat : 로드 인덱스 수정

--- Assets/Scripts/Manager/Manager.cs
+++ Assets/Scripts/Manager/Manager.cs
@@ -31,6 +31,6 @@ public static class Manager
         SceneManager.CreateInstance();
         SoundManager.CreateInstance();
         UIManager.CreateInstance();
-        Data.LoadData();
+        Data.LoadData(1);
     }
 }
--- Assets/_Scripts/Janggi/JanggiScene.cs
+++ Assets/_Scripts/Janggi/JanggiScene.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,13 +13,30 @@ public class JanggiScene : BaseScene
 {
     public override IEnumerator LoadingRoutine()
     {
-        Manager.Data.LoadData(0);
-        //Manager.Data.GameData.pieceData
+        Manager.Data.LoadData(0);        
         yield return null;
     }
 
     public void SceneChange(string sceneName)
     {
+        DataSave();
         Manager.Scene.LoadScene(sceneName);
+    }
+
+    void DataSave()
+    {
+        List<Spot> spotList = new List<Spot>();
+
+        foreach (Spot spot in Manager.JanggiLogic.JanggiLogicSituation)
+        {
+            if (spot.OnPiece)
+            {
+                spotList.Add(spot);
+            }
+        }
+
+        Manager.Data.GameData.pieceData.PieceSave(spotList);
+        Manager.Data.GameData.isSaved = true;
+        Manager.Data.SaveData(1);
     }
 }

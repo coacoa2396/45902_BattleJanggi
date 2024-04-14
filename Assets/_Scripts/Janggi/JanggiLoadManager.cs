7774538fb5badb95a03d73d1b24c9bddc15e07f0
Yu <kgadbcksrb@gmail.com>
2024-04-15 08:56:11 +0900

    feat : 분기 안땄다! fps에서 장기로 돌아오는거 제작중

---
+++ Assets/_Scripts/Janggi/JanggiLoadManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JanggiLoadManager : MonoBehaviour
+{
+    [SerializeField] JanggiLogic janggiLogic;
+
+    public void FPStoJanggi()
+    {
+        Manager.Data.LoadData(1);
+        PieceData data = Manager.Data.GameData.pieceData;
+        foreach (PiecePosData piece in data.pieces)
+        {
+            switch ()
+        }
+    }
+}

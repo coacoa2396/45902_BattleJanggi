74bd38fad98cedd6b94ea42d3e48c896fe4ed11f
Yu <kgadbcksrb@gmail.com>
2024-04-15 15:42:50 +0900

    feat : FPS UI 자료 추가

---
+++ Assets/ReadMe4.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//1P와 2P가 각각 원하는 말 배치 버튼을 눌렀을 때
+//버튼에 해당하는 CheckImage의 Visible을 True로 바꾸면 됩니다
+//CloseButton을 눌렀을 경우 GameStart Scene으로 복귀하면 됩니다
+public class ReadMe4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

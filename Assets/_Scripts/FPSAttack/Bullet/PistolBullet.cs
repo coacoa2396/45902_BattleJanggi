6cca282d6b947ded27c5cc92265622bcbf85d12f
Mellling <154119653+Mellling@users.noreply.github.com>
2024-04-23 16:31:57 +0900

    fix: FPS씬 장기말(졸) 총 중간에 터지는 버그 수정

--- Assets/_Scripts/FPSAttack/Bullet/PistolBullet.cs
+++ Assets/_Scripts/FPSAttack/Bullet/PistolBullet.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class PistolBullet : Bullet
 {
     [SerializeField] PistolImpact explodeEffect;        // 탄환이 닿았을 때 나오는 이팩트
+    [SerializeField] LayerMask playerCheck;         // 플레이어 체크하는 레이어마스크
 
     protected override void OnEnable()
     {
@@ -17,7 +18,46 @@ public class PistolBullet : Bullet
 
     private void FixedUpdate()
     {
+        RaycastHit[] hits;
+
         Vector3 nextPos = transform.position + Rigid.velocity * Time.fixedDeltaTime;
+        hits = Physics.RaycastAll(transform.position, transform.forward, Vector3.Distance(transform.position, nextPos));
+
+        foreach (RaycastHit hit in hits)
+        {
+            if (playerCheck.Contain(hit.transform.gameObject.layer))
+            {
+                FPSPiece target;
+                hit.collider.gameObject.TryGetComponent<FPSPiece>(out target);
+
+                target?.TakeDamage(Damage);
+
+                Manager.Pool.GetPool(explodeEffect, hit.point, Quaternion.LookRotation(hit.normal));
+
+                gameObject.SetActive(false);
+                return;
+            }
+
+            Collider[] colls = hit.transform.gameObject.GetComponents<Collider>();
+
+            foreach (Collider coll in colls)
+            {
+                if (coll.isTrigger)
+                {
+                    break;
+                }
+                else
+                {
+                    Manager.Pool.GetPool(explodeEffect, hit.point, Quaternion.LookRotation(hit.normal));
+
+                    gameObject.SetActive(false);
+
+                    return;
+                }
+            }
+        }
+
+        /*Vector3 nextPos = transform.position + Rigid.velocity * Time.fixedDeltaTime;
         if (Physics.Linecast(transform.position, nextPos, out RaycastHit hitInfo))
         {
             FPSPiece target;
@@ -28,7 +68,7 @@ public class PistolBullet : Bullet
             Manager.Pool.GetPool(explodeEffect, transform.position, Quaternion.LookRotation(hitInfo.normal));
 
             gameObject.SetActive(false);
-        }
+        }*/
     }
 
     private void OnDisable()

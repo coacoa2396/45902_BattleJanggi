d728d153d3a079c8ce717a6cd5c88140a4e4ec2e
YuChanGyu <kgadbcksrb@gmail.com>
2024-04-29 17:00:19 +0900

    feat : 빌드 정리

--- Assets/_Scripts/Title/SaveMaker.cs
+++ Assets/_Scripts/Title/SaveMaker.cs
@@ -7,11 +7,11 @@ using UnityEngine;
 
 public class SaveMaker : MonoBehaviour
 {
+    [SerializeField] TextAsset firstSet;
+
     private GameData gameData;
     public GameData GameData { get { return gameData; } }
-
-    private string setPath => Path.Combine(Application.dataPath, $"Resources/Data/FirstSet");
-
+        
 #if UNITY_EDITOR
     private string path => Path.Combine(Application.dataPath, $"Resources/Data/SaveLoad");
 #else
@@ -20,15 +20,20 @@ public class SaveMaker : MonoBehaviour
 
     private void Start()
     {
+        // 0번 세이브가 존재하는지 확인
         if (Manager.Data.ExistData(0))
             return;
 
         // FisrtSet을 불러와서 GameData에 저장한다
-        string json = File.ReadAllText($"{setPath}/FirstSet.txt");
+        //string json = File.ReadAllText($"{setPath}/FirstSet.txt");
+        string json = firstSet.text;
+        if (json == null)
+            Debug.LogError("Json is null!!");
+
         gameData = JsonUtility.FromJson<GameData>(json);
 
         // 이 GameData를 다시 0번 Save로 만든다
-        if (Directory.Exists(path) == false)
+        if (Directory.Exists(path) == false)        // 폴더가 있는지 확인한다
         {
             Directory.CreateDirectory(path);
         }
